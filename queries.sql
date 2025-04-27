eda9f96fb52cfa5afa3468ba6b7642696ce6ba94
JASH <jash@192.168.1.7>
2025-04-27 10:05:55 +0530

    Updated queries.sql

--- queries.sql
+++ queries.sql
@@ -1,19 +1,29 @@
--- books table
-CREATE TABLE books(
-	id SERIAL PRIMARY KEY,
-	isbn TEXT,
-	title TEXT NOT NULL,
-	author TEXT,
-	description TEXT,
-	review TEXT,
-	rating INTEGER CHECK (rating >= 0 AND rating <= 5),
-	image_path TEXT,
-	date_read DATE
-)
+-- Users table
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  email VARCHAR(255) UNIQUE NOT NULL,
+  password VARCHAR(255) NOT NULL
+);
 
--- notes table
-CREATE TABLE notes(
-	id SERIAL PRIMARY KEY,
-	note TEXT NOT NULL,
-	book_id INTEGER REFERENCES books(id) ON DELETE CASCADE
+-- Books table
+CREATE TABLE books (
+  id SERIAL PRIMARY KEY,
+  isbn VARCHAR(20),
+  title VARCHAR(255) NOT NULL,
+  author VARCHAR(255) NOT NULL,
+  description TEXT,
+  review TEXT,
+  rating INTEGER,
+  image_path VARCHAR(255),
+  date_read TIMESTAMP,
+  user_id INTEGER,
+  FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+-- Notes table
+CREATE TABLE notes (
+  id SERIAL PRIMARY KEY,
+  note TEXT,
+  book_id INTEGER NOT NULL,
+  FOREIGN KEY (book_id) REFERENCES books(id) ON DELETE CASCADE
 );
